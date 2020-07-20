ac36e3ff269cf7889b02d0ae3a7eae5fc60173d3
Alessandro Aurora <alx.aurora@hotmail.it>
2020-07-20 18:06:12 +0200

    Spark streaming set up

--- covid_tweet_analysis/spark_streaming.py
+++ covid_tweet_analysis/spark_streaming.py
@@ -1,27 +1,38 @@
-from pyspark import SparkConf,SparkContext
+from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.sql import Row,SQLContext
+from pyspark.sql import Row, SQLContext, SparkSession
 import sys
 import requests
-
+from pprint import pprint
 
 # CONTEXT DEFINITION
 # create spark configuration
-conf = SparkConf()
-conf.setAppName("TwitterStreamAppCovid")
+spark = SparkSession.builder\
+    .appName("TwitterStreamAppCovid")\
+    .master("local[2]")\
+    .getOrCreate() 
 
-# create spark context with the above configuration
-sc = SparkContext(conf=conf)
+sc = spark.sparkContext
+
+# create the Streaming Context from the above spark context with the specified interval size seconds
+ssc = StreamingContext(sc, 5)
 sc.setLogLevel("ERROR")
-
-# create the Streaming Context from the above spark context with interval size 2 seconds
-ssc = StreamingContext(sc, 2)
 
 # setting a checkpoint to allow RDD recovery
 ssc.checkpoint("checkpoint_TwitterApp")
 
 # read data from port 9009
-dataStream = ssc.socketTextStream("localhost",9009)
+dataStream = ssc.socketTextStream("localhost", 9009)
+
+
+# Sets the session as a global variable
+def getSparkSessionInstance(sparkConf):
+    if ('sparkSessionSingletonInstance' not in globals()):
+        globals()['sparkSessionSingletonInstance'] = SparkSession \
+            .builder \
+            .config(conf=sparkConf) \
+            .getOrCreate()
+    return globals()['sparkSessionSingletonInstance']
 
 
 # TRANSFORMATION LOGIC
@@ -31,45 +42,32 @@ def aggregate_tags_count(new_values, total_sum):
     return sum(new_values) + (total_sum or 0)
 
 
-def get_sql_context_instance(spark_context):
-    if ('sqlContextSingletonInstance' not in globals()):
-        globals()['sqlContextSingletonInstance'] = SQLContext(spark_context)
-    return globals()['sqlContextSingletonInstance']
-
-
+#SparkStreaming
 def process_rdd(time, rdd):
     print("----------- %s -----------" % str(time))
     try:
-        # Get spark sql singleton context from the current context
-        sql_context = get_sql_context_instance(rdd.context)
+        # Get spark session singleton context from the current context
+        session = getSparkSessionInstance(rdd.context.getConf())
         # convert the RDD to Row RDD
         row_rdd = rdd.map(lambda w: Row(hashtag=w[0], hashtag_count=w[1]))
         # create a DF from the Row RDD
-        hashtags_df = sql_context.createDataFrame(row_rdd)
+        hashtags_df = session.createDataFrame(row_rdd)
         # Register the dataframe as table
-        hashtags_df.registerTempTable("hashtags")
+        hashtags_df.createOrReplaceTempView("hashtags")
         # get the top 10 hashtags from the table using SQL and print them
-        hashtag_counts_df = sql_context.sql("select hashtag, hashtag_count from hashtags order by hashtag_count desc limit 10")
+        hashtag_counts_df = session.sql("select hashtag, hashtag_count from hashtags order by hashtag_count desc limit 10")
         hashtag_counts_df.show()
-        # call this method to prepare top 10 hashtags DF and send them
-        send_df_to_dashboard(hashtag_counts_df)
     except:
         e = sys.exc_info()[0]
         print("Error: %s" % e)
 
 
-def send_df_to_dashboard(df):
-    # extract the hashtags from dataframe and convert them into array
-    top_tags = [str(t.hashtag) for t in df.select("hashtag").collect()]
-    # extract the counts from dataframe and convert them into array
-    tags_count = [p.hashtag_count for p in df.select("hashtag_count").collect()]
-    # initialize and send the data through REST API
-    url = 'http://localhost:5001/updateData'
-    request_data = {'label': str(top_tags), 'data': str(tags_count)}
-    response = requests.post(url, data=request_data)
-
-
-# def save_tweets_into_hdfs(path)
+def send_df_to_database(df):
+    spark.write\
+        .format("org.apache.spark.sql.cassandra")\
+        .mode('append')\
+        .options(table="kv", keyspace="test")\
+        .save().show()
 
 
 # split each tweet into words
@@ -80,7 +78,7 @@ hashtags = words.filter(lambda w: '#' in w).map(lambda x: (x, 1))
 
 # adding the count of each hashtag to its last count
 # tags_totals = hashtags.updateStateByKey(aggregate_tags_count)
-tags_totals = hashtags.reduceByKey(lambda a, b: a + b)
+tags_totals = hashtags.reduceByKeyAndWindow(func=(lambda a, b: a + b), invFunc=None, windowDuration=20)
 
 # do processing for each RDD generated in each interval
 tags_totals.foreachRDD(process_rdd)
